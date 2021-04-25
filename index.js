d460e5995a74e70b85d218826f3c1abadbedfe68
sheilapolichetti <77405590+sheilapolichetti@users.noreply.github.com>
2021-04-25 18:13:47 +0200

    first assignment

---
+++ index.js
@@ -0,0 +1,74 @@
+/* 
+
+Case 1 
+
+const baseValue = ("10");
+const heightValue = ("13");
+
+// calculate the area
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(
+  `The area of the triangle is ${areaValue}` 
+); 65
+
+
+const baseValue = ("16.5");
+const heightValue = ("20.3")
+
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(
+    `The area of the triangle is ${areaValue}`
+); 167.475
+
+`Triangle One has an area of 65cm2 and Triangle Two has an area of 167.475cm2. The triangle with the biggest area is Triangle two.`
+
+Case two
+
+
+const baseValue = ("16.5");
+const heightValue = ("20.3")
+
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(
+    `The area of the triangle is ${areaValue}`
+);
+
+
+const baseValue = ("20.3");
+const heightValue = ("16.5")
+
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(
+    `The area of the triangle is ${areaValue}`
+);
+
+`Triangle three has an area of 167.475cm2 and Triangle four has an area of 167.475cm2. Both triangles have equally big areas.
+
+
+const baseValue = ("7.8");
+const heightValue = ("5.6")
+
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(
+    `The area of the triangle is ${areaValue}`
+); 21.84
+
+
+const baseValue = ("9.3");
+const heightValue = ("12.4")
+
+const areaValue = (baseValue * heightValue) / 2;
+
+console.log(
+    `The area of the triangle is ${areaValue}`
+); 57.660000000000004
+
+
+`Triangle five has an area of 21.84cm2 and Triangle six has an area of 57.660000000000004cm2. The triangle with the biggest area is 
+triangle six.
+*/
