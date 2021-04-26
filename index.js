e6e3cd1e53f331081601cc68d3178a2f1113a9ae
sheilapolichetti <77405590+sheilapolichetti@users.noreply.github.com>
2021-04-26 14:32:09 +0200

    fixed again with expression

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ const triangleTwo = (16.5 * 20.3) / 2
 const isBigger = triangleOne >= triangleTwo;
 
 if (isBigger) {
-    console.log("triangleOne has a bigger/equal area than/as triangleTwo")
+    console.log("triangleOne has a bigger/as big area as triangleTwo", {triangleOne, triangleTwo})
 } else {
-    console.log("triangleTwo has a bigger area than triangleOne")
+    console.log("triangleTwo has a bigger area than triangleOne", {triangleTwo, triangleOne})
 }
 
 //Case two
 
-const triangleThree = (16.5 * 13) / 2;
+const triangleThree = (16.5 * 20.3) / 2;
 const triangleFour = (20.3 * 16.5) / 2;
 
 const biggerArea = triangleThree >= triangleFour;
 
 if (biggerArea) {
-    console.log("triangleThree has a bigger area than triangleFour")
+    console.log("triangleThree has a bigger/as big area as triangleFour", {triangleThree, triangleFour})
 } else {
-    console.log("triangleFour has a bigger/as big area as triangleThree")
+    console.log("triangleFour has a bigger area as triangleThree", {triangleFour, triangleThree})
 };
 
 // Case three 
@@ -36,10 +36,8 @@ if (biggerArea) {
 const triangleFive = (7.8 * 5.6) / 2;
 const triangleSix = (9.3 * 12.4) / 2;
 
-const areaBigger = triangleFive >= triangleSix;
-
-if (areaBigger) {
-    console.log("triangleFive has a bigger/equal area than/as triangleSix")
+if (areaBigger = triangleFive >= triangleSix) {
+    console.log("triangleFive has a bigger/as big area as triangleSix", {triangleFive, triangleSix})
 } else {
-    console.log("triangleSix has a bigger area than triangleFive")
+    console.log("triangleSix has a bigger area than triangleFive", {triangleSix, triangleFive})
 };
